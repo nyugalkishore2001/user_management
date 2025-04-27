588718a5f116303261377805accb4583fbcb9269
Mickey Mouse <mickeymouse@mac.myfiosgateway.com>
2025-04-27 19:24:38 -0400

    config file change

--- settings/config.py
+++ settings/config.py
@@ -22,8 +22,8 @@ class Settings(BaseSettings):
     refresh_token_expire_minutes: int = 1440  # 24 hours for refresh token
     # Database configuration
     database_url: str = Field(default='postgresql+asyncpg://user:password@postgres/myappdb', description="URL for connecting to the database")
-    database_url_asyncpg: str = Field(..., description="Database URL for AsyncPG")
-
+    database_url_asyncpg: str = Field(default='postgresql+asyncpg://user:password@postgres/myappdb', description="Database URL for AsyncPG")
+    
     # Optional: If preferring to construct the SQLAlchemy database URL from components
     postgres_user: str = Field(default='user', description="PostgreSQL username")
     postgres_password: str = Field(default='password', description="PostgreSQL password")
